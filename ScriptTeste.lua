175ae2315063b787a519a0389d2e2e5a5f38c0eb
Hyder <94196770+HyderMoon@users.noreply.github.com>
2021-11-12 18:31:33 -0300

    Update ScriptTeste.lua

--- ScriptTeste.lua
+++ ScriptTeste.lua
@@ -18,17 +18,16 @@ end)
 
 repeat wait() until game.Players.LocalPlayer
 
-m = game.Players.LocalPlayer:GetMouse()
+evt = game.Players.LocalPlayer:GetMouse()
 
-m.KeyDown:connect(function(key)
-	if key == "0" then --"Shift to run" 0 == shift
-		print("Running")
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 32 --change "25" to your speed you want
-	end
+evt.KeyDown:connect(function(key)
+    if key == "0" then
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 25
+    end
 end)
 
-m.KeyUp:connect(function(key)
-	if key == "0" then
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16 --change "16" to your speed you want when you stop running
-	end
+evt.KeyUp:connect(function(key)
+    if key == "0" then
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 15
+    end
 end)
