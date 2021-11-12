fe89010893b2cf261de040b33b37c553925848ae
Hyder <94196770+HyderMoon@users.noreply.github.com>
2021-11-12 17:45:03 -0300

    Update ScriptTeste.lua

--- ScriptTeste.lua
+++ ScriptTeste.lua
@@ -15,3 +15,21 @@ mo.KeyDown:Connect(function(k)
 		p.Character.Humanoid:ChangeState(11)
 	end
 end)
+
+mouse.KeyDown:connect(function (key) -- Run function
+	key = string.lower(key)
+	if string.byte(key) == 48 then
+		running = true
+		local keyConnection = mouse.KeyUp:connect(function (key)
+			if string.byte(key) == 48 then
+				running = false
+			end
+		end)
+
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 25
+		repeat wait () until running == false
+		keyConnection:disconnect()
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+		
+	end
+end)
