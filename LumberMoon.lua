2fba9479161f2c04e471bfb9ff1fca5ed5ac2413
Hyder <94196770+HyderMoon@users.noreply.github.com>
2021-11-16 13:50:30 -0300

    Update LumberMoon.lua

--- LumberMoon.lua
+++ LumberMoon.lua
@@ -10,136 +10,37 @@ local UICorner = Instance.new("UICorner")
 local Fly = Instance.new("TextButton")
 local UICorner_2 = Instance.new("UICorner")
 Fly.MouseButton1Down:connect(function()
-	repeat wait() 
-	until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Head") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid") 
-	local mouse = game.Players.LocalPlayer:GetMouse() 
-	repeat wait() until mouse
-	local plr = game.Players.LocalPlayer 
-	local torso = plr.Character.Head 
-	local flying = false
-	local deb = true 
-	local ctrl = {f = 0, b = 0, l = 0, r = 0} 
-	local lastctrl = {f = 0, b = 0, l = 0, r = 0} 
-	local maxspeed = 400 
-	local speed = 5000 
+	local player = game.Players.LocalPlayer
+	local char = player.Character or player.CharacterAdded:Wait()
+	local h = char:WaitForChild('Humanoid')
+	local torso = char:WaitForChild('LowerTorso')
 
-	function Fly() 
-		local bg = Instance.new("BodyGyro", torso) 
-		bg.P = 9e4 
-		bg.maxTorque = Vector3.new(9e9, 9e9, 9e9) 
-		bg.cframe = torso.CFrame 
-		local bv = Instance.new("BodyVelocity", torso) 
-		bv.velocity = Vector3.new(0,0.1,0) 
-		bv.maxForce = Vector3.new(9e9, 9e9, 9e9) 
-		repeat wait() 
-			plr.Character.Humanoid.PlatformStand = true 
-			if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then 
-				speed = speed+.5+(speed/maxspeed) 
-				if speed > maxspeed then 
-					speed = maxspeed 
-				end 
-			elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then 
-				speed = speed-1 
-				if speed < 0 then 
-					speed = 0 
-				end 
-			end 
-			if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then 
-				bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
-				lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r} 
-			elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then 
-				bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
-			else 
-				bv.velocity = Vector3.new(0,0.1,0) 
-			end 
-			bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0) 
-		until not flying 
-		ctrl = {f = 0, b = 0, l = 0, r = 0} 
-		lastctrl = {f = 0, b = 0, l = 0, r = 0} 
-		speed = 0 
-		bg:Destroy() 
-		bv:Destroy() 
-		plr.Character.Humanoid.PlatformStand = false 
-	end 
-	mouse.KeyDown:connect(function(key) 
-		if key:lower() == "r" then 
-			if flying then flying = false 
-			else 
-				flying = true 
-				Fly() 
-			end 
-		elseif key:lower() == "w" then 
-			ctrl.f = 1 
-		elseif key:lower() == "s" then 
-			ctrl.b = -1 
-		elseif key:lower() == "a" then 
-			ctrl.l = -1 
-		elseif key:lower() == "d" then 
-			ctrl.r = 1 
-		end 
-	end) 
-	mouse.KeyUp:connect(function(key) 
-		if key:lower() == "w" then 
-			ctrl.f = 0 
-		elseif key:lower() == "s" then 
-			ctrl.b = 0 
-		elseif key:lower() == "a" then 
-			ctrl.l = 0 
-		elseif key:lower() == "d" then 
-			ctrl.r = 0 
-		end 
+	local uis = game:GetService('UserInputService')
+	local mouse = player:GetMouse()
+
+
+	local flying = false
+
+	uis.InuputBegan:Connect(function(input)
+		if input.keyCode == Enum.KeyCode.F then
+			if not flying then
+				local bodyVelocity = Instance.new('BodyVelocity', torso)
+				flying = true
+				while flying do
+					bodyVelocity.Velocity = mouse.Hit.lookVector * 100
+					wait()
+				end
+			else
+				flying = false
+				torso:FindFirstChildOfClass('BodyVelocity') :Destroy()
+			end  
+		end
 	end)
-	Fly()	
+end)
 local NoClip = Instance.new("TextButton")
 local UICorner_3 = Instance.new("UICorner")
-NoClip.MouseButton1Down:connect(function()
-	noclip = false
-	game:GetService('RunService').Stepped:connect(function()
-		if noclip then
-			game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-		end
-	end)
-	plr = game.Players.LocalPlayer
-	mouse = plr:GetMouse()
-	mouse.KeyDown:connect(function(key)
-
-		if key == "e" then
-			noclip = not noclip
-			game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-		end
-	end)
-end)
 local Player = Instance.new("TextLabel")
 local InfJump = Instance.new("TextButton")
-InfJump.MouseButton1Down:connect(function()
-	_G.infinjump = true
-
-	local Player = game:GetService("Players").LocalPlayer
-	local Mouse = Player:GetMouse()
-	Mouse.KeyDown:connect(function(k)
-		if _G.infinjump then
-			if k:byte() == 32 then
-				Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-				Humanoid:ChangeState("Jumping")
-				wait(0.1)
-				Humanoid:ChangeState("Seated")
-			end
-		end
-	end)
-
-	local Player = game:GetService("Players").LocalPlayer
-	local Mouse = Player:GetMouse()
-	Mouse.KeyDown:connect(function(k)
-		k = k:lower()
-		if k == "f" then
-			if _G.infinjump == true then
-				_G.infinjump = false
-			else
-				_G.infinjump = true
-			end
-		end
-	end)
-end)
 local UICorner_4 = Instance.new("UICorner")
 local WaterWalk = Instance.new("TextButton")
 local UICorner_5 = Instance.new("UICorner")
@@ -300,7 +201,7 @@ UICorner_9.Parent = TeleportLocations
 
 -- Scripts:
 
-local function FAXTW_fake_script() -- ScrollingFrame.LocalScriptMoverJanela 
+local function BBWH_fake_script() -- ScrollingFrame.LocalScriptMoverJanela 
 	local script = Instance.new('LocalScript', ScrollingFrame)
 
 	local UIS = game:GetService('UserInputService')
@@ -338,4 +239,4 @@ local function FAXTW_fake_script() -- ScrollingFrame.LocalScriptMoverJanela
 		end
 	end)
 end
-coroutine.wrap(FAXTW_fake_script)()
+coroutine.wrap(BBWH_fake_script)()
